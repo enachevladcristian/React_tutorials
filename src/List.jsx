dd4e83baad308b55cbb70721606251f289494a95
enachevladcristian <enachevlad31@yahoo.com>
2024-11-25 14:56:01 +0200

    Tutorial 1

---
+++ src/List.jsx
@@ -0,0 +1,16 @@
+function List(){
+    return(
+        <>
+        <p>De ce folosesc React? : </p>
+        <ul>
+            <li>Este un limbaj popular</li>
+            <li>Ma ajuta sa imi fac teza de doctorat</li>
+            <li>Are un logo super tare</li>
+            <li>XD</li>
+        </ul>
+        </>
+    )
+}
+
+
+export default List
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import List from './List.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <List />
+  </StrictMode>,
+)
